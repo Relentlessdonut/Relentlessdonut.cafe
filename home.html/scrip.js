97a7ae349c38d36f60072d3af9397ba2eb4131bc
Moises <moises072010@gmail.com>
2026-02-21 00:45:23 -0500

    Updated Whole Website

--- home.html/scrip.js
+++ home.html/scrip.js
@@ -1,15 +1,6 @@
-const header = document.querySelector('header');
-const hambuergerbtn = document.querySelector
-('#hamburger-btn');
-const closeMenuBtn =document.querySelector
-('#close-menu-btn');
+const toggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
 
-// Toggle moblie menut on hamburger button click
-
-hambuergerbtn.addEventListener('click' , ()=> header.
-    classList.toggle('show-moblie-menu'));
-
-// Close moblie menu on close button click
-
-closeMenuBtn.addEventListener('click', ()=>
-hambuergerbtn.click());
+toggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
